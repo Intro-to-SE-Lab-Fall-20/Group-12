b9839b26ba54ea9a3dac173cb4db2147c031f983
Hunter Mitchell <contact@hunter.dev>
2020-09-25 18:56:22 -0500

    Changes

--- routes.js
+++ routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+const { RequireAuth } = require("./middleware");
 
 // Register Routes for the MemeMail API
 router.get("/", (req, res) => {
-
+    if (req.isAuthenticated()) {
+        return res.redirect("/inbox");
+    }es
     return res.render("pages/index");
 });
 
@@ -15,5 +18,15 @@ router.get("/auth/google", passport.authenticate("google", {
 }));
 
 router.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/", successRedirect: "/inbox" }));
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    return res.redirect("/");
+})
+
+// Other Routes
+router.get("/inbox", RequireAuth, (req, res) => {
+    return res.render("pages/inbox", { user: req.user });
+});
 
 module.exports = router;
